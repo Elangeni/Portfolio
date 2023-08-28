74f7d903144a13a724767ccd6612da6ec312c4bc
Elangeni <elangeniyabba96@gmail.com>
2023-08-27 23:56:17 -0400

    initial commiy

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import HeaderLinks from './components/header/HeaderLinks';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app'>
+        <div class="header">
+          <div class="menu-circle"></div>
+          <HeaderLinks/>
+          <img class="profile-img" src="" alt=""/>
+          
+        </div>
+        
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/header/HeaderLinks.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './HeaderLinks.css'
+
+const HeaderLinks = () => {
+  return (
+    <div className='header-menu'>
+        <a class="menu-link is-active main-header-link" href="#">About Me</a>
+        <a class="menu-link " href="#">Portfolio</a>
+    </div>
+  )
+}
+
+export default HeaderLinks
